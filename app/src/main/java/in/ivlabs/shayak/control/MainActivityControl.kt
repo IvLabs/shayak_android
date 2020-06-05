fb100e8cb07cc0415e5f85d2584f3800fc5478d3
Ritik <agrawalritik27@gmail.com>
2020-06-05 13:41:59 +0530

    layout completed

---
+++ app/src/main/java/in/ivlabs/shayak/control/MainActivityControl.kt
@@ -0,0 +1,12 @@
+package `in`.ivlabs.shayak.control
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ivlabs.shayak.R
+
+class MainActivityControl : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_control)
+    }
+}
