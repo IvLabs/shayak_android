9ce9412e3517f11cab19f083e8a126483b432fbb
Ajinkya Kamat <kamat.ajinkya@gmail.com>
2020-09-06 19:34:27 +0530

    WIP: Client Server

---
+++ app/src/main/java/com/ivlabs/shayak/RobotRunningActivity.kt
@@ -0,0 +1,20 @@
+package com.ivlabs.shayak
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class RobotRunningActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_robot_running)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
